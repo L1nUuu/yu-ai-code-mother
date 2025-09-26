2c888baa1f757d974a21c2492bb2091979d3a99f
LYNN <2935546973@qq.com>
2025-09-26 08:52:35 +0800

    11-实现SSE流式输出

--- src/test/java/com/lynn/yuaicodemother/core/AiCodeGeneratorFacadeTest.java
+++ src/test/java/com/lynn/yuaicodemother/core/AiCodeGeneratorFacadeTest.java
@@ -5,8 +5,10 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 import java.io.File;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,4 +31,17 @@ class AiCodeGeneratorFacadeTest {
         File file = aiCodeGeneratorFacade.generatorAndSaveCode("生成一个管理系统页面，不超过100行代码", CodeGenTypeEnum.MULTI_FILE);
         Assertions.assertNotNull(file);
     }
+
+    @Test
+    void generateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("生成一个类似若依的管理系统，总共不超过100行代码", CodeGenTypeEnum.MULTI_FILE);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        // 拼接字符串，得到完整内容
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
+
 }
